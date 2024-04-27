9d62c4e41f8ed4bbe8c0caaf985c66714d502e25
faceslog <66552663+faceslog@users.noreply.github.com>
2024-04-27 20:39:38 +0200

    Add small model for test

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # federated-learning (DOING)
 Small project to better understand: https://arxiv.org/pdf/1602.05629 McMahan et al., “Communication-Efficient Learning of Deep Networks from Decentralized Data”, 2016
+
+
+**Arguments and Parameters of FedAvg**
+
+- **Number of Clients (K)**: Total number of clients that own local datasets.
+- **Fraction of Clients (C)**: Fraction of clients that are randomly selected to participate in each round of training.
+- **Local Epochs (E)**: Number of epochs each client should train on its local data before sending updates back to the server.
+- **Batch Size (B)**: The size of the mini-batches used for local training on each client.
+- **Learning Rate (η)**: The learning rate used by clients for local optimization.
--- src/shared/model.py
+++ src/shared/model.py
@@ -1,38 +1,39 @@
 import torch
 from torchvision.models import resnet18, ResNet18_Weights
 import torch.nn as nn
-from typing import Type
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 class ModelManager:
     def __init__(self):
         self.model = self.load_model()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
+        self.train_dl = None
+        self.val_dl = None
+
+    # ===========================================================
 
     def load_model(self):
 
         model = resnet18(weights=ResNet18_Weights.DEFAULT)
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, 2) 
-        model.add_module('sigmoid', nn.Sigmoid()) 
+
+        # Free resnet layers
+        for param in model.parameters():
+            param.requires_grad = False
+        
+        model.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
+        model.fc = nn.Sequential(nn.Flatten(), nn.Linear(512, 128), nn.ReLU(), nn.Dropout(0.2), nn.Linear(128, 1), nn.Sigmoid())
         
         return model
     
+    # ===========================================================
+    
     def get_model(self):
-        """Returns the current model."""
         return self.model
-
-    def update_model(self, updates):
-        """
-        Applies federated averaging to update the model's weights using the updates provided by the clients.
-
-        Params:
-            updates (list of torch.Tensor): A list containing the weight tensors from each client's model updates.
-        """
-        with torch.no_grad():
-            # Average each parameter across all client updates
-            for param, update in zip(self.model.parameters(), updates):
-                param.data = torch.mean(torch.stack([param.data] + update), 0)
+    
+    # ===========================================================       
     
     def get_model_weights(self):
         """
@@ -42,6 +43,8 @@ class ModelManager:
             list of torch.Tensor: A list of tensors representing the weights of the model.
         """
         return [param.data for param in self.model.parameters()]
+    
+    # ===========================================================
 
     def set_model_weights(self, weights):
         """
@@ -53,3 +56,77 @@ class ModelManager:
         with torch.no_grad():
             for param, weight in zip(self.model.parameters(), weights):
                 param.data.copy_(weight)
+
+    # ===========================================================
+
+    def create_dataloader(self, data_dir, batch_size):
+        """Load data from disk and apply transformations."""
+        transform = transforms.Compose([
+            transforms.Resize((256, 256)),  # Resize images to 256x256
+            transforms.ToTensor(),  # Convert images to tensor
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize images
+        ])
+        
+        dataset = datasets.ImageFolder(root=data_dir, transform=transform)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+        return dataloader
+    
+    # ===========================================================
+    def train_epoch(self, dataloader, optimizer, criterion):
+
+        self.model.train()
+        total_loss, total_accuracy = 0, 0
+
+        for inputs, labels in dataloader:
+            inputs, labels = inputs.to(self.device), labels.to(self.device).float().view(-1, 1)
+
+            optimizer.zero_grad()
+            outputs = self.model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+            predictions = (outputs > 0.5).float()
+            total_accuracy += (predictions == labels).float().mean().item()
+
+        return total_loss / len(dataloader), total_accuracy / len(dataloader)
+
+    # ===========================================================
+    def validate_epoch(self, dataloader, criterion):
+
+        self.model.eval()
+
+        with torch.no_grad():
+            total_loss, total_accuracy = 0, 0
+
+            for inputs, labels in dataloader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device).float().view(-1, 1)
+
+                outputs = self.model(inputs)
+                loss = criterion(outputs, labels)
+        
+                total_loss += loss.item()
+                predictions = (outputs > 0.5).float()
+                total_accuracy += (predictions == labels).float().mean().item()
+
+            return total_loss / len(dataloader), total_accuracy / len(dataloader)
+    # ===========================================================
+
+    def train(self, train_dir, val_dir, num_epochs, learning_rate, batch_size):
+
+        if not self.train_dl:
+            self.train_dl = self.create_dataloader(train_dir, batch_size)
+
+        if not self.val_dl:
+            self.val_dl = self.create_dataloader(val_dir, batch_size)
+
+        criterion = nn.BCELoss()
+        optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+        
+        for epoch in range(num_epochs):
+            train_loss, train_acc = self.train_epoch(self.train_dl, optimizer, criterion)
+            val_loss, val_acc = self.validate_epoch(self.val_dl, criterion)
+            print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}')
+
